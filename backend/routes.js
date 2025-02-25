678da3b00ef9b6f59c9e7c148a9eac46f61c8c5c
iFangg <i.Fangg@proton.me>
2025-02-25 15:45:57 +1100

    Updated routes stub for nextjs accidentally pasted in a nodejs stub

--- backend/routes.js
+++ backend/routes.js
@@ -7,21 +7,30 @@ const pool = mariadb.createPool({
 });
 console.log('is this called ?');
 
-async function asyncFunction() {
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     let conn;
     try {
+
       conn = await pool.getConnection();
       const rows = await conn.query("show tables;");
-      console.log(rows); //[ {val: 1}, meta: ... ]
-    //   const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-    //   console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-  
+      // rows: [ {val: 1}, meta: ... ]
+
+    //   const rowsData = await conn.query('SELECT 1 + 1 AS solution');
+      console.log('tables: ', rows);
+    //   res.status(200).json(rowsData);
+
+      // const dbResponse = await conn.query(`INSERT INTO myTable value (?, ?)`, [1, "mariadb"]);
+      // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+      // res.status(200).json(dbResponse);
+
     } catch (err) {
+      console.log('error here : ', err);
       throw err;
     } finally {
-      if (conn) conn.end();
+      if (conn) {
+        conn.release(); //release to pool
+      }
     }
   }
-  asyncFunction().then(() => {
-    pool.end();
-  });
+}
