5bfd3ebdc2b662530766c8e28ef86e0c64a0f60e
iFangg <i.Fangg@proton.me>
2025-02-25 15:44:09 +1100

    Updated fullstack updated gitignore to be stored in root dir added small character changes to test stability of base frontend code added stub for backend routes added .env file to store db login data udpated db dockerfile

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,27 @@
 const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: `${process.env.DB_HOSTNAME}`,
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PWD}`,
+    connectionLimit: 2
+});
+console.log('is this called ?');
+
+async function asyncFunction() {
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query("show tables;");
+      console.log(rows); //[ {val: 1}, meta: ... ]
+    //   const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+    //   console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) conn.end();
+    }
+  }
+  asyncFunction().then(() => {
+    pool.end();
+  });
