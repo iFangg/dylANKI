cc2e95ebb4d891c84396154cae41c4ecf4869158
Ivan Fang <iFang.py@gmail.com>
2025-02-27 22:24:56 +1100

    Added css folder for more thorough organisation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../css/globals.css";
+import "../css/header.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import "../../db/queries";
-import { TestButton } from "./components/TestButton";
-import { AddFlashcardButton } from "./components/AddFlashcard";
+import { TestButton } from "../components/ui/TestButton";
+import { AddFlashcardButton } from "../components/ui/AddFlashcard";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header>
+        Header test
+      </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
