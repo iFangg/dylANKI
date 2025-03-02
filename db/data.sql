1702cbb3426921d90fc7efc69c0b41a0a9df127a
iFangg <i.Fangg@proton.me>
2025-03-02 12:29:52 +1100

    Cleaning codebase + editing dockerfiles for better practice + edited data.sql added in semi colons in data.sql for correctness

--- db/data.sql
+++ db/data.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS dylanki.Flashcards (
 	DateCreated DATETIME NOT NULL,
 	DateLastModified DATETIME NOT NULL,
 	CONSTRAINT Flashcard_ID PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE IF NOT EXISTS dylanki.Decks (
 	ID INT auto_increment NOT NULL,
@@ -17,18 +17,18 @@ CREATE TABLE IF NOT EXISTS dylanki.Decks (
 	DateCreated DATETIME NOT NULL,
 	DateLastModified DATETIME NOT NULL,
 	CONSTRAINT Deck_ID PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE IF NOT EXISTS dylanki.CardInDeck (
 	FlashcardID INT NOT NULL,
 	DeckID INT NOT NULL,
 	CONSTRAINT Flashcard_ID FOREIGN KEY (FlashcardID) REFERENCES dylanki.Flashcards(ID),
 	CONSTRAINT Deck_ID FOREIGN KEY (DeckID) REFERENCES dylanki.Decks(ID)
-)
+);
 
 CREATE TABLE dylanki.DeckInDeck (
 	DeckID INT NOT NULL,
 	SuperDeckID INT NOT NULL,
 	CONSTRAINT fk_Deck_ID FOREIGN KEY (DeckID) REFERENCES dylanki.Decks(ID),
 	CONSTRAINT fk_Super_Deck_ID FOREIGN KEY (SuperDeckID) REFERENCES dylanki.Decks(ID)
-)
+);
