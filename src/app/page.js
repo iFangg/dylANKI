f9468e8d33f60d96ac43ea5ce4c2fbdac8bb966e
iFangg <i.Fangg@proton.me>
2025-03-02 15:58:54 +1100

    Changed navbar to be a fixed component

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import { AddDeckButton } from "@/components/ui/AddDeck";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="header w-full">
+      <header className="header w-full fixed">
         <div className="container mx-auto">
-          <Navbar/>
+          <Navbar className="w-full"/>
         </div>
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
