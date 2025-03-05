0146faa0e41c402a8cf8cc82c248bf03477b90dc
iFangg <i.Fangg@proton.me>
2025-03-05 23:47:58 +1100

    Added classNames to main page sections

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="main-page flex gap-8">
-          <section>Test 1</section>
-          <section>Test 2</section>
+          <section className="curr-Deck-view">Test 1</section>
+          <section className="deck-list">Test 2</section>
         </div>
         {/* <Image
           className="dark:invert"
