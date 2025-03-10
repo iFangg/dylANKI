69e20904924cbdfb22b6192c2547d45482a9d1e6
iFangg <i.Fangg@proton.me>
2025-03-10 12:58:12 +1100

    Changed padding space for sections and updated all decks route in navbar

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     // change this to later fit underneath the navbar
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-start justify-center w-full h-full">
-        <div className="main-page flex gap-8 items-center ml-8 sm:ml-16">
+        <div className="main-page flex gap-[200px] items-center ml-8 sm:ml-16">
           <section className="curr-Deck-view">
             <DeckView />
           </section>
--- src/components/sections/navbar/default.jsx
+++ src/components/sections/navbar/default.jsx
@@ -1,9 +1,3 @@
-// import Navigation from "../../ui/navigation";
-// import { Button } from "../../ui/button";
-// import { Sheet, SheetContent, SheetTrigger } from "../../ui/sheet";
-// import { Menu } from "lucide-react";
-// import LaunchUI from "../../logos/launch-ui";
-// import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
 "use client"
 
 import {
@@ -26,18 +20,8 @@ export default function Navbar() {
       <div className="relative mx-auto max-w-container">
         <NavbarComponent>
           <NavbarLeft>
-            {/* <a href="/" className="flex items-center gap-2 text-xl font-bold">
-              <LaunchUI />
-              Launch UI
-            </a>
-            <Navigation /> */}
-             {/* <Link href="/decklist" legacyBehavior passHref>
-              <a className={navigationMenuTriggerStyle({ className: '' })}>
-                Deck List
-              </a>
-            </Link> */}
             {/* fix hover issue (goes away after one click)*/}
-            <Link href="/decklist" className={navigationMenuTriggerStyle()}>
+            <Link href="/allDecks" className={navigationMenuTriggerStyle()}>
               <div className="flex flex-col" id="parent">
                 <div id="child1" className="block center">
                   <Image
@@ -95,33 +79,6 @@ export default function Navbar() {
                 </div>
               </div>
             </Link>
-            {/* <Button variant="default" asChild>
-              <a href="/">Get Started</a>
-            </Button>
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="ghost" size="icon" className="shrink-0 md:hidden">
-                  <Menu className="h-5 w-5" />
-                  <span className="sr-only">Toggle navigation menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="right">
-                <nav className="grid gap-6 text-lg font-medium">
-                  <a href="/" className="flex items-center gap-2 text-xl font-bold">
-                    <span>Launch UI</span>
-                  </a>
-                  <a href="/" className="text-muted-foreground hover:text-foreground">
-                    Getting Started
-                  </a>
-                  <a href="/" className="text-muted-foreground hover:text-foreground">
-                    Components
-                  </a>
-                  <a href="/" className="text-muted-foreground hover:text-foreground">
-                    Documentation
-                  </a>
-                </nav>
-              </SheetContent>
-            </Sheet> */}
           </NavbarRight>
         </NavbarComponent>
       </div>
