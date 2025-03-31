f8c752e710e3eeec4ecbecbc3a9a07836e3c1468
Ivan Fang <iFang.py@gmail.com>
2025-03-31 21:10:18 +1100

    Added pop up form for adding deck

--- src/app/allDecks/page.js
+++ src/app/allDecks/page.js
@@ -1,5 +1,19 @@
+"use client"
+
 import { AddDeckButton } from "@/components/ui/AddDeck";
 import { DeckList } from "@/components/ui/DeckList";
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import Image from "next/image";
 
 export default function allDecks() {
@@ -10,8 +24,38 @@ export default function allDecks() {
           <div className="relative">
             <DeckList width="1400px" height="575px"/>
           </div>
-          <div className="ml-4 mb-[31px]">
-            <AddDeckButton />
+          <div className="ml-4">
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="add-deck-button shadow-lg" variant="outline">
+                    <Image
+                        src="/plus.svg"
+                        width = {50}
+                        height = {50}
+                        alt="Add Deck"
+                    />
+                    </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you're done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="Deck Title" className="text-right">
+                                Deck Title
+                            </Label>
+                            <Input id="name" value="Deck Name" className="col-span-3" onChange={() => {}}/>
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
           </div>
         </div>
       </main> 
